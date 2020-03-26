3548e82513dc516daa7432d32c22ea84e9efa5ec
Jeremy Iverson <jiverson002@csbsju.edu>
2020-03-25 19:59:15 -0500

    CMake: Overhaul

---
+++ src/gc/realloc.c
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: MIT */
+#include <stdlib.h>
+
+#include "efika/core/gc.h"
+
+/*----------------------------------------------------------------------------*/
+/*! Reallocate an allocation. */
+/*----------------------------------------------------------------------------*/
+extern void*
+GC_realloc_impl(
+  unsigned const gc_ctr,
+  unsigned const gc_pctr,
+  void **gc_ptr,
+  void ***gc_pptr,
+  void *ptr,
+  size_t const n
+)
+{
+  if (NULL == ptr)
+    return NULL;
+
+  for (unsigned i = 0; i < gc_ctr; i++)
+    if (ptr == gc_ptr[i])
+      return gc_ptr[i] = realloc(ptr, n);
+
+  for (unsigned i = 0; i < gc_pctr; i++)
+    if (ptr == *(gc_pptr[i]))
+      return gc_pptr[i] = realloc(ptr, n);
+
+  return NULL;
+}
--- src/include/efika/core/gc.h
+++ src/include/efika/core/gc.h
@@ -20,13 +20,15 @@
   , __gc_ptr[__gc_ctr++]\
   );\
   if (__gc_ret)\
-    return -1
+    return -1;\
+  else\
+    (void)0
 
 #define GC_assert(cond)\
   if (!(cond)) {\
     GC_cleanup();\
     return -1;\
-  }
+  } else (void)0
 
 #define GC_cleanup()\
   GC_cleanup_impl(__gc_ctr, __gc_pctr, __gc_ptr, __gc_pptr)
@@ -39,18 +41,22 @@
 
 #define GC_malloc(n)     __gc_alloc(malloc(n))
 #define GC_calloc(n, sz) __gc_alloc(calloc(n, sz))
+#define GC_realloc(xptr, n)\
+  GC_realloc_impl(__gc_ctr, __gc_pctr, __gc_ptr, __gc_pptr, xptr, n)
 
 /*----------------------------------------------------------------------------*/
 /*! Bypass the garbage collector. */
 /*----------------------------------------------------------------------------*/
-#define unsafe_malloc malloc
-#define unsafe_calloc calloc
-#define unsafe_free   free
+#define unsafe_malloc  malloc
+#define unsafe_calloc  calloc
+#define unsafe_realloc realloc
+#define unsafe_free    free
 
 /*----------------------------------------------------------------------------*/
 /*! Private API. */
 /*----------------------------------------------------------------------------*/
-void GC_cleanup_impl(unsigned, unsigned, void **, void ***);
-void GC_free_impl(unsigned, unsigned, void **, void ***, void *);
+void  GC_cleanup_impl(unsigned, unsigned, void **, void ***);
+void  GC_free_impl(unsigned, unsigned, void **, void ***, void *);
+void* GC_realloc_impl(unsigned, unsigned, void **, void ***, void *, size_t);
 
 #endif /* EFIKA_CORE_GC_H */
